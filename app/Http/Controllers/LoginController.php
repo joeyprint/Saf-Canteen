75efa7ceebaeec32a0f8e2ad3ac30dfe583f9782
Joey Wasin <wasin.wj@mail.kmutt.ac.th>
2017-11-21 08:28:34 +0700

    EDIT Login Controller and ADD Register Controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -9,12 +9,30 @@ class LoginController extends Controller
     public function custLogin(Request $request){
         $user = $request->username;
         $pass = $request->password;
-        return view('userPages.authen');
+        $userLogin = DB::table('customers')
+            ->select('username', 'password')
+            ->where(['username','=', $user], ['password', '=', $pass])
+            ->get();
+
+            return view('userPages.authen');
     }
 
     public function organLogin(Request $request){
         $user = $request->username;
         $pass = $request->password;
+        $organLogin = DB::table('staffs')
+        ->select('username', 'password')
+        ->where(['username','=', $user], ['password', '=', $pass])
+        ->get();
+
         return view('orgPages.authen');
+    }
+
+    public function custLogout(){
+        return redirect('/');
+    }
+
+    public function organLogout(){
+        return redirect('/organ');
     }
 }
