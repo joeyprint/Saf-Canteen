d70586a992487aaf557db58e3e791df0a7f29d44
basskt02 <basza604@hotmail.com>
2017-11-23 17:39:07 +0700

    ADD function in MenuController

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -6,5 +6,30 @@ use Illuminate\Http\Request;
 
 class MenuController extends Controller
 {
-    //
+    public function showBurgerMenu() {
+        $burgerMenu = DB::table('menus')
+                ->select('menuName')
+                ->where(['categoryId', '=', 1])
+                ->get();
+        return view('userPages.burgerMenu');
+    }
+
+    public function showChickAndSnackMenu() {
+        $chickenMenu = DB::table('menus')
+                ->select('menuName')
+                ->where(['categoryId', '=', 2])
+                ->get();
+        $snackMenu = DB::table('menus')
+                ->select('menuName')
+                ->where(['categoryId', '=', 3])
+        return view('userPages.chickAndSnackMenu');
+    }
+
+    public function showDessertMenu() {
+        $dessertMenu = DB::table('menus')
+                ->select('menuName')
+                ->where(['categoryId', '=', 4])
+                ->get();
+        return view('userPages.dessertMenu');
+    }
 }
