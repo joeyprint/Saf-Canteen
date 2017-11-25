3d3f83e3138925b27a1209ce8b9e6bf05f37a08f
basskt02 <basza604@hotmail.com>
2017-11-25 15:35:20 +0700

    MOCK ADD pic

--- resources/views/userPages/burgerMenu.blade.php
+++ resources/views/userPages/burgerMenu.blade.php
@@ -1,26 +1,28 @@
 @extends('layouts.user')
 
 @section('title')
-	Burger Menu
+  Burger
 @endsection
 
 @section('custom_head')
-	<link rel="stylesheet" href="/css/userStyle/menu.css">
+  <link rel="stylesheet" href="/css/userStyle/menu.css">
 @endsection
 
 @section('content')
-	<div class="container">
-		<div class="row">
+<div class="container">
+    <div class="row">
 			<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+
 				<div class="jumbotron">
-					<div class="container">
-						<h1>หัวข้อ</h1>
-						<p>Contents ...</p>
-						<p>
-							<a class="btn btn-primary btn-lg">ใส่ตระกร้า</a>
-						</p>
-					</div>
-				</div>
+				    <div class="container">
+				           <h1>หัวข้อ</h1>
+                            <p>Contents ...</p>
+                            <p>
+								<a class="btn btn-primary btn-lg">ใส่ตระกร้า</a>            
+                            </p>
+									
+                        </div>
+					</div>				
 			</div>
 			<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
 				<div class="jumbotron">
@@ -32,6 +34,7 @@
 						</p>
 					</div>
 				</div>
+				
 			</div>
 			<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
 				<div class="jumbotron">
@@ -43,7 +46,9 @@
 						</p>
 					</div>
 				</div>
+				
 			</div>
 		</div>
-	</div>
+</div>
+  
 @endsection
--- resources/views/userPages/snackMenu.blade.php
+++ resources/views/userPages/snackMenu.blade.php
@@ -6,14 +6,31 @@
 
 @section('custom_head')
 	<link rel="stylesheet" href="/css/userStyle/menu.css">
+	
+
 @endsection
 
 @section('content')
 	<div class="container">
 		<div class="row">
-			<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+			<div class="col-xs-12 col-md-4">
+				<div class="jumbotron">
+					<div class="row" >
+                       <img src="/img/snackMenu/frenchfries.png" alt="">
+                        
+						<h1>หัวข้อ</h1>
+						<p>Contents ...</p>
+                        
+						<p>
+							<a class="btn btn-primary btn-lg">ใส่ตระกร้า</a>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-md-4">
 				<div class="jumbotron">
 					<div class="container">
+                        <img src="/img/snackMenu/Hash Browns.png" alt="">
 						<h1>หัวข้อ</h1>
 						<p>Contents ...</p>
 						<p>
@@ -22,20 +39,12 @@
 					</div>
 				</div>
 			</div>
-			<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+			<div class="col-xs-12 col-md-4">
 				<div class="jumbotron">
 					<div class="container">
-						<h1>หัวข้อ</h1>
-						<p>Contents ...</p>
-						<p>
-							<a class="btn btn-primary btn-lg">ใส่ตระกร้า</a>
-						</p>
-					</div>
-				</div>
-			</div>
-			<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-				<div class="jumbotron">
-					<div class="container">
+                        <p>
+                        <img src="/img/snackMenu/frenchfries.png" alt="">
+                        </p>
 						<h1>หัวข้อ</h1>
 						<p>Contents ...</p>
 						<p>
